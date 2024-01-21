f29351a46c9cc6ebb3fd4077de0e11e2b7e0eebf
Victor Campos <victor.htp@hotmail.com>
2024-01-21 07:05:56 -0300

    chore: Configs and libraries

---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+// import { UserRole } from "@prisma/client"
+// import NextAuth from "next-auth"
+
+// export type ExtendedUser = DefaultSession["user"] & {
+//   role: UserRole
+//   customField: string
+// }
+
+// declare module "next-auth" {
+//   interface Session {
+//     user: ExtendedUser
+//   }
+// }
